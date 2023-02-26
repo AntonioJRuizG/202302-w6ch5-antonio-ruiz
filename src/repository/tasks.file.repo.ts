2ffa13ecde48cbd92fd73837cbe84eae98848b7b
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-26 18:31:03 +0100

    Add repo methods

--- src/repository/tasks.file.repo.ts
+++ src/repository/tasks.file.repo.ts
@@ -24,12 +24,21 @@ export class TaskFileRepo {
     await fs.writeFile(file, appendData, { encoding: 'utf-8' });
   }
 
-  async update(newData: TaskStructure['id']) {
+  async update(newData: TaskStructure) {
     const info = await fs.readFile(file, { encoding: 'utf-8' });
     const parsedData: TaskStructure[] = JSON.parse(info);
-    const nextIdNumber: number = parsedData.length;
-    newData.id = nextIdNumber;
-    const appendData = JSON.stringify([...parsedData, newData]);
-    await fs.writeFile(file, appendData, { encoding: 'utf-8' });
+    const updatedData = parsedData.map((item) =>
+      item.id === newData.id ? newData : item
+    );
+    const finalData = JSON.stringify(updatedData);
+    await fs.writeFile(file, finalData, { encoding: 'utf-8' });
+  }
+
+  async delete(newData: TaskStructure) {
+    const info = await fs.readFile(file, { encoding: 'utf-8' });
+    const parsedData: TaskStructure[] = JSON.parse(info);
+    const deleteData = parsedData.splice(newData.id);
+    const finalData = JSON.stringify(deleteData);
+    await fs.writeFile(file, finalData, { encoding: 'utf-8' });
   }
 }
